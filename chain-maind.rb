8c0c3c0ea7bb8731b62773b6402a7eba421bee28
Leslie Cheung <51898510+lezzokafka@users.noreply.github.com>
2020-11-09 12:04:53 +0800

    0.7.0_rc1_brew_update (#1)

--- chain-maind.rb
+++ chain-maind.rb
@@ -1,16 +1,16 @@
 class ChainMaind < Formula
   desc "chain-main daemon"
   homepage "https://github.com/crypto-com/chain-main"
-  version "0.7.0-rc0"
+  version "0.7.0-rc1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/crypto-com/chain-main/releases/download/v0.7.0-rc0/chain-main_0.7.0-rc0_Darwin_x86_64.tar.gz"
-    sha256 "1c03c12c4b2248cfd2287411c2925fdce49edc595d2f26941b7b979ee1b227af"
+    url "https://github.com/crypto-com/chain-main/releases/download/v0.7.0-rc1/chain-main_0.7.0-rc1_Darwin_x86_64.tar.gz"
+    sha256 "4a0baca2815a76adb039d4fc168a40814ed55b98effc89281b32796858b07fd7"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/crypto-com/chain-main/releases/download/v0.7.0-rc0/chain-main_0.7.0-rc0_Linux_x86_64.tar.gz"
-      sha256 "f791cf60cbee5fac42a67ce0f98d8db7f20fae126dde33c7341c857ffbb66478"
+      url "https://github.com/crypto-com/chain-main/releases/download/v0.7.0-rc1/chain-main_0.7.0-rc1_Linux_x86_64.tar.gz"
+      sha256 "43eaa8f6b9f75f2ea3d19eb8b2e88292ffac9871e590be33cc9bf974d435e1ab"
     end
   end
 
