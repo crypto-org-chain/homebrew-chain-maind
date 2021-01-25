63e67b168678e5a2d3a7846db9a47e30a41abec9
Leslie Cheung <leslie@crypto.com>
2021-01-25 11:51:47 +0800

    v0.81

--- chain-maind.rb
+++ chain-maind.rb
@@ -1,16 +1,16 @@
 class ChainMaind < Formula
   desc "chain-main daemon"
   homepage "https://github.com/crypto-com/chain-main"
-  version "0.8.0-croeseid"
+  version "0.8.1-croeseid"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/crypto-com/chain-main/releases/download/v0.8.0-croeseid/chain-main_0.8.0-croeseid_Darwin_x86_64.tar.gz"
-    sha256 "d3c7c8923a41c21dcab80295b08cebe4d7b1ed0ff3e48894cfdaca88e7a3380e"
+    url "https://github.com/crypto-com/chain-main/releases/download/v0.8.1-croeseid/chain-main_0.8.1-croeseid_Darwin_x86_64.tar.gz"
+    sha256 "5ecbb953361a91cc34ca9d6a64f38ad4959f2e507a6d4d1fc7a4f5bcca97e906"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/crypto-com/chain-main/releases/download/v0.8.0-croeseid/chain-main_0.8.0-croeseid_Linux_x86_64.tar.gz"
-      sha256 "36002779f5a7b2a5766079ca5e4736e47ad03d3f39b87bc4fc15e466cf12e4c9"
+      url "https://github.com/crypto-com/chain-main/releases/download/v0.8.1-croeseid/chain-main_0.8.1-croeseid_Linux_x86_64.tar.gz"
+      sha256 "49bb6ded4f2454af63b698451a4f445739ebafa035da8d39abffc2595b0255de"
     end
   end
 
