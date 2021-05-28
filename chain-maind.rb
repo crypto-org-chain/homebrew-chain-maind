64534a4d46309af6eddd9f7dd10b5ef7132a9e23
Leslie H. Cheung <51898510+lezzokafka@users.noreply.github.com>
2021-05-28 18:13:20 +0800

    Update chain-maind.rb
    Co-authored-by: Leo Pang <34628052+allthatjazzleo@users.noreply.github.com>

--- chain-maind.rb
+++ chain-maind.rb
@@ -7,7 +7,10 @@ class ChainMaind < Formula
   if OS.mac?
     url "https://github.com/crypto-org-chain/chain-main/releases/download/v2.0.1/chain-main_2.0.1_Darwin_x86_64.tar.gz"
   elsif OS.linux?
-    if Hardware::CPU.intel?
+    if Hardware::CPU.arm?
+      url "https://github.com/crypto-org-chain/chain-main/releases/download/v2.0.1/chain-main_2.0.1_Linux_arm64.tar.gz"
+      sha256 "345c7eacfc768df355c3b3ecadc9cc39e3c1656c9c0c4d465b938834fa66ff03"
+    else
       url "https://github.com/crypto-org-chain/chain-main/releases/download/v2.0.1/chain-main_2.0.1_Linux_x86_64.tar.gz"
       sha256 "5e9e9f703cb85c72573086e384e187e752463b2ed0ccd612094a1f29a13f0158"
     end
